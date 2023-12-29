59af130ffbdcf1cc0dab6c0eab1418a2d88fb0a7
hayatul-islam <hayatulemon340.com>
2023-12-29 23:45:47 +0600

    base url change

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://worklopedia-2.onrender.com/api/admin",
+    baseUrl: "https://assignment-tcd5.onrender.com/",
     credentials: "include",
   }),
   tagTypes: ["login"],
