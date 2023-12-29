e699e2f6c536d695c48ce4d14cb56a2bdeac27b7
hayatul-islam <hayatulemon340.com>
2023-12-29 23:55:10 +0600

    base url change

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://assignment-tcd5.onrender.com/",
+    baseUrl: "https://assignment-tcd5.onrender.com/api/admin/",
     credentials: "include",
   }),
   tagTypes: ["login"],
